4dedefbfc23d51085b17e929ea57250c754b1e2e
Caia Chuang <caiachuang@gmail.com>
2021-05-12 14:01:42 -0700

    Import CSS for shards.

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
 
 const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
 
