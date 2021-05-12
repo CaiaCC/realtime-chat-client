d53c2b980a2cde52c8149c684893191276e78e71
Caia Chuang <caiachuang@gmail.com>
2021-05-12 14:38:15 -0700

    Create Message component & display messages from backend.

--- src/Chat.js
+++ src/Chat.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider, useQuery, gql, useQuery } from "@apollo/client";
+
 
 const client = new ApolloClient({
     uri: "http://localhost:4000/",
     cache: new InMemoryCache(),
 });
 
+const GET_MESSAGES = gql`
+    query {
+        messages {
+            id
+            content
+            user
+        }
+    }
+`;
+
+const Message = ({ user }) => {
+    const { data } = useQuery(GET_MESSAGES);
+    if (!data) {
+        return null;
+    }
+    return JSON.stringify(data);
+}
+
 const Chat = () => {
-    return <div>Chat Window</div>
+    return (
+        <div>
+            <Message user="Caia" />
+        </div>
+    )
 };
 
 export default () => (
