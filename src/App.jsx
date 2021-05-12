2015f3b6c625e67ca7f3b7d9a75d8e36e3d6efe2
Caia Chuang <caiachuang@gmail.com>
2021-05-12 14:09:41 -0700

    Add Chat component & implement ApolloProvidert.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "shards-ui/dist/css/shards.min.css";
+import Chat from "./Chat";
 
-const App = () => <div>Hi there, I'm React from Webpack 5.</div>;
+const App = () => <Chat />;
 
 ReactDOM.render(<App />, document.getElementById("app"));
